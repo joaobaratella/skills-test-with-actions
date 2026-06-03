ba2491e8580d68ebc75e7b24091ad06eba5b2688
joaobaratella <127469266+joaobaratella@users.noreply.github.com>
2026-06-03 13:54:42 +0000

    teste1

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -47,15 +47,15 @@ def test_get_nth_fibonacci_zero():
 
 
 def test_get_nth_fibonacci_ten():
- """Test with n=10."""
- # Arrange
- n = 10
+    """Test with n=10."""
+    # Arrange
+    n = 10
 
- # Act
- result = get_nth_fibonacci(n)
- 
- # Assert
- assert result == 55
+    # Act
+    result = get_nth_fibonacci(n)
+
+    # Assert
+    assert result == 55
 
 
 # def test_get_nth_fibonacci_ten():
